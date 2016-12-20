e57a94ea14a124eeb663995e3f3228b2dc6ea6a2
Magnus Bengtsson <magnus.bengtsson@expressen.se>
2016-12-20 13:28:23 +0100

    Small fixes for the stub

--- vault.go
+++ vault.go
@@ -1,24 +1,27 @@
 package vault_jwt
 
-"github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+)
 
 // Implements the transit secret backend in Vault for signing.
 
 type SigningMethodVault struct{}
 
-
-
 func init() {
-    jwt.RegisterSigningMethod("Vault", func() jwt.SigningMethod {
-        return &SigningMethodVault{}
-    })
+	jwt.RegisterSigningMethod("Vault", func() jwt.SigningMethod {
+		return &SigningMethodVault{}
+	})
 }
 
+func (s *SigningMethodVault) Alg() string {
+	return "Vault" // Non-standard!
+}
 
 func (s *SigningMethodVault) Sign(signingString string, key interface{}) (string, error) {
-
+	return "", nil
 }
 
 func (s *SigningMethodVault) Verify(signingString, signature string, key interface{}) error {
-
+	return nil
 }
