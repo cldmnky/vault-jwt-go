2025bc12d5bdaf00565475864471373a6241b4ab
Magnus Bengtsson <magnus.bengtsson@expressen.se>
2016-12-22 12:40:07 +0100

    Add vault client

--- vault.go
+++ vault.go
@@ -1,8 +1,51 @@
 package vault_jwt
 
 import (
+	"crypto/tls"
+	"log"
+	"net/http"
+
 	"github.com/dgrijalva/jwt-go"
+	"github.com/hashicorp/vault/api"
+	"github.com/imdario/mergo"
 )
+
+type Config struct {
+	VaultConfig *api.Config
+	Path        string
+	Token       string
+	SSLEnabled  bool
+}
+
+func newVaultClient(config *Config) (*api.Client, error) {
+	log.Printf("[INFO] (vault_jwt) creating vault/api client")
+	// Create the default config object
+	vaultConfig := api.DefaultConfig()
+
+	// merge configs
+	if err := mergo.Merge(&vaultConfig, config.VaultConfig); err != nil {
+		log.Printf("[ERROR] (vault_jwt) could not merge configs", err)
+		return nil, err
+	}
+	if config.SSLEnabled {
+		tlsConfig := &tls.Config{}
+		vaultConfig.HttpClient.Transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	}
+	// return client
+	client, err := api.NewClient(vaultConfig)
+	if err != nil {
+		return nil, err
+	}
+	if config.Token != "" {
+		log.Printf("[DEBUG] (vault_jwt) setting vault token")
+		client.SetToken(config.Token)
+	} else {
+		log.Fatalln("[ERROR] (vault_jwt) no vault token")
+	}
+	return client, nil
+}
 
 // Implements the transit secret backend in Vault for signing.
 
